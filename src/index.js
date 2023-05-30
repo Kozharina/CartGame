97c05889f074c4bfdc10b40e7868fa4f423ce31f
Kozharina <120336880+Kozharina@users.noreply.github.com>
2023-05-30 23:31:08 +0000

    2 и 3 страница 3-course-homework

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { renderingDifficulty } from "./components/difficulty-component.js";
+
+// eslint-disable-next-line no-undef
+globalThis = {
+    mainElement: document.getElementById("main"),
+};
+
+renderingDifficulty();
